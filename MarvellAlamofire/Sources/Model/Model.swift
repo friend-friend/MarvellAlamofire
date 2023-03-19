d0642fcc4a63b145018b92525ff508e1cac03a8b
Vyacheslav Onuchin <sla651@mail.ru>
2023-03-19 22:40:08 +1000

    Created model.

--- MarvellAlamofire/Sources/Model/Model.swift
+++ MarvellAlamofire/Sources/Model/Model.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+struct MarvellAnswer: Codable {
+    let data: Characters?
+}
+
+struct Characters: Codable {
+    let results: [Character]
+}
+
+struct Character: Codable {
+    let id: Int?
+        let name, description: String?
+        let thumbnail: Image?
+        let comics: Comics?
+}
+
+struct Comics: Codable {
+    let available: Int
+    let items: [ComicsItem]
+}
+
+struct ComicsItem: Codable {
+    let name: String
+}
+
+struct Image: Codable {
+    let path: String?
+    let format: String?
+
+    enum CodingKeys: String, CodingKey {
+        case path
+        case format = "extension"
+    }
+}
